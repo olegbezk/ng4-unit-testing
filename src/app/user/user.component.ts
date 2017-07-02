3b6be1ebd96881ade053650a92dac5719ce0c82d
olegbezkorovanynyi <oleg.bezkorovaynyi@gmail.co>
2017-07-02 19:15:54 +0300

    add component and template for test

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
+  user: {name: string};
+  isLoggedIn = false;
 
   constructor() { }
 
